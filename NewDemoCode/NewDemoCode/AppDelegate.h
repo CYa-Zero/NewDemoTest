3d215070cf7ade82ca7bf5c61b81326c4bad8ebb
Lonely-Sword <chenyang.on.no@gmail.com>
2016-07-20 12:30:10 -0500

    Two labels
    two labels added

---
+++ NewDemoCode/NewDemoCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDemoCode
+//
+//  Created by Chenyang on 7/20/16.
+//  Copyright © 2016 Chenyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
